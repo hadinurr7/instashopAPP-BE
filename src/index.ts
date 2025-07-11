32977ce9bd909c229612dba27d7600a901c76076
hadinurr7 <hadinurr7@gmail.com>
2025-07-12 05:57:51 +0700

    feat: user-details

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
 import authRouter from "./routes/auth.router";
 import { PORT } from "./config";
+import userRouter from "./routes/user.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/auth", authRouter);
+app.use("/users", userRouter)
 
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
